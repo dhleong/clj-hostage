a7bf463d583563ec46ff9dbe1f82addcb9e72890
Daniel Leong <me@dhleong.net>
2024-07-22 10:03:43 -0400

    Support skipping steps by tag
    Tags can be skipped when running via cli by using an argument of the
    form `--skip-<tag>`, eg `--skip-tests` to skip steps tagged as `:tests`.
    
    Tags override `:always-run?`, enabling you to have steps that normally
    always run even with `--dry-run`, but which may be skipped by explicitly
    specifying their tag

--- src/hostage/flow.clj
+++ src/hostage/flow.clj
@@ -3,12 +3,21 @@
    [babashka.process :as process]
    [clojure.string :as str]))
 
-(defonce ^:dynamic *dry-run?* false)
 (defonce ^:dynamic *debug* false)
+(defonce ^:dynamic *disabled-tags* #{})
+(defonce ^:dynamic *dry-run?* false)
+
+(defn extract-disabled-tags [args]
+  (let [prefix "--skip-"]
+    (->> args
+         (filter #(str/starts-with? % prefix))
+         (map #(keyword (subs % (count prefix))))
+         (into #{}))))
 
 (defmacro execute [opts & body]
-  `(binding [*dry-run?* (:dry-run? ~opts)
-             *debug* (:debug ~opts)]
+  `(binding [*debug* (:debug ~opts)
+             *disabled-tags* (:disabled-tags ~opts)
+             *dry-run?* (:dry-run? ~opts)]
      (try
        ~@body
        (catch Throwable e#
@@ -21,21 +30,23 @@
 #_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (defmacro main [& body]
   `(do (defn ~'-main [& args#]
-         (let [opts# {:dry-run? (some #{"--dry-run"} args#)
-                      :debug (some #{"--debug"} args#)}]
+         (let [opts# {:debug (some #{"--debug"} args#)
+                      :dry-run? (when (some #{"--dry-run"} args#)
+                                  "dry-run")
+                      :disabled-tags (extract-disabled-tags args#)}]
            (execute opts# ~@body)))
 
        (when (= *file* (System/getProperty "babashka.file"))
          (apply ~'-main *command-line-args*))))
 
-(defn run-step [{:keys [always-run? name]} f]
-  ; TODO: Support skipping by :tag
-  (let [should-run? (or (not *dry-run?*)
-                        always-run?)]
-    (binding [*dry-run?* (not should-run?)]
-      (if should-run?
+(defn run-step [{:keys [always-run? name tag]} f]
+  (let [skip-reason (or (when (and *dry-run?* (not always-run?))
+                          "dry-run")
+                        (*disabled-tags* tag))]
+    (binding [*dry-run?* skip-reason]
+      (if-not skip-reason
         (println "[run]" name)
-        (println "[skip - dry run]" name))
+        (println (str "[skip - " skip-reason "]") name))
       (f))))
 
 #_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
@@ -54,7 +65,7 @@
 (defn shell [command & args]
   (println
    (if *dry-run?*
-     "[skip - dry-run]"
+     (str "[skip - " *dry-run?* "]")
      "[run]")
    "shell: " (->> (into [command] args)
                   (remove map?)))
